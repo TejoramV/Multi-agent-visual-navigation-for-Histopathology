c442aae94ead0c5c04b84417adef37884e5fa6df
TejoramV <42198973+TejoramV@users.noreply.github.com>
2024-04-03 14:10:05 -0700

    Update playground.py

--- playground.py
+++ playground.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import openai
 import backoff
+import re
+import csv
 
 # Setup OpenAI for Azure    
 os.environ["OPENAI_API_KEY"] = ''
@@ -46,9 +48,40 @@ if __name__ == "__main__":
     directory = 'C:/Users/stlp/Desktop/Liinda/GPT/' 
     gpt_model= "gpt35"
     max_tokens =50 
-    # message= """clue: [1. The description of the cuboidal epithelium as a square, symmetrical structure with a centrally located nucleus, 2. The mention of the cuboidal epithelium's function in glandular epithelium and its role in the transport of ions like sodium and potassium, 3. The identification of the cuboidal epithelium in the sweat glands, renal system's collecting tubules, proximal collecting ducts, and distal collecting ducts, 4. The description of the simple squamous epithelium as flattened cells, 5. The identification of the simple squamous epithelium in the thin limbs of the loop of Henle in the kidney.]"""
     message ="""clues: [1. Presence of scattered dark firm areas on multiple lobes consistent with locally extensive areas of bronchopneumonia, 2. Hyperkeratosis of the foot pad and nose, 3. Conjunctivitis and enamel hypoplasia, 4. Microscopic examination showing approximately 95% of the lung is diffusely hemorrhagic with scattered areas of necrosis, 5. Airways plugged with cellular exudate and the lining epithelium is hyperplastic, 6. Alveolar architecture replaced by marked inflammation, hemorrhage, and fibrin with edema, 7. Presence of abundant degenerate and non-degenerate neutrophils, macrophages, fibrin deposits, and abundant hemorrhage, 8. Less affected alveoli are lined by type 2 pneumocyte hyperplasia, 9. Pneumocytes frequently fuse together and form multinucleated viral syncytia containing up to 10 nuclei, 10. Presence of eosinophilic intracytoplasmic and intranuclear viral inclusion bodies, 11. Presence of viral syncytia along a combination of intracytoplasmic and intranuclear viral inclusions is consistent with canine distemper virus.]"""
-    # print(message)
-    response = io_gpt(message, gpt_model=gpt_model, max_tokens =max_tokens)
-    findings_string = response['choices'][0]['message']['content']
-    print(findings_string)
+    
+    # Slice the DataFrame to get rows 3 to 20 (inclusive)
+    subset_data = data.iloc[10:310]
+    result = []
+
+        
+    # Iterate through each row in the subset
+    for index, row in subset_data.iterrows():
+        pattern = r'clues:'
+        match = re.search(pattern, row['content'])
+        if match:
+            index_location = match.start()
+            # print(row['content'][index_location+7:-1]) #clues without "clues:"
+            message = row['content'][index_location:-1] #clues    
+            try:
+                response = io_gpt(message, gpt_model=gpt_model, max_tokens =max_tokens)
+            except:
+                continue    
+            findings_string = response['choices'][0]['message']['content']
+
+            print('Video ID:', row['video_id'])
+            print(row['content'][12:index_location-2])# GPT 4 diagnosis  
+            print(findings_string[11:]) # GPT 3.5 diagnosis
+
+            result.append([row['video_id'], findings_string[11:], row['content'][12:index_location-2],row['content'][index_location+7:-1]])
+        else:
+            continue
+    csv_file_path = "GPT3.5vs4.csv"
+    with open(csv_file_path, mode='w', newline='') as file:
+        # Create a CSV writer object
+        writer = csv.writer(file)
+        writer.writerow(["video_id","gpt_3.5_diagnosis","gpt_4_diagnosis","clues"])
+        # Append the data to the CSV file
+        for row in result:
+            writer.writerow(row)
+    print("Data appended successfully.")
